f3fb668801813246a1edcae39072913fd9ad5441
Jez <jthompson@lincoln.ac.uk>
2017-12-13 15:55:49 +0000

    Remove figure inline styles function

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -30,3 +30,40 @@ function hello_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'hello_pingback_header' );
+
+
+/**
+ * Remove Inline Styles From Figures
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+
+
+function remove_img_caption_style($attr, $content = null) {
+		// New-style shortcode with the caption inside the shortcode with the link and image tags.
+		if ( ! isset( $attr['caption'] ) ) {
+			if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+				$content = $matches[1];
+				$attr['caption'] = trim( $matches[2] );
+			}
+		}
+		// Allow plugins/themes to override the default caption template.
+		$output = apply_filters('img_caption_shortcode', '', $attr, $content);
+		if ( $output != '' )
+			return $output;
+		extract(shortcode_atts(array(
+			'id'	  => '',
+			'align'	  => 'alignnone',
+			'width'	  => '',
+			'caption' => ''
+		), $attr));
+		if ( 1 > (int) $width || empty($caption) )
+			return $content;
+		if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+		return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" style="width: auto">'
+		. do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
+	
+add_shortcode('wp_caption', 'remove_img_caption_style');
+add_shortcode('caption', 'remove_img_caption_style');
