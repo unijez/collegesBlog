dd869add0c7f79a9b6d4bfe91deae3d2288c7297
Jez <jthompson@lincoln.ac.uk>
2017-12-13 11:48:19 +0000

    Fix mess up

--- comments.php
+++ comments.php
@@ -7,7 +7,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package hello
+ * @package schools
  */
 
 /*
@@ -31,13 +31,13 @@ if ( post_password_required() ) {
 			if ( 1 === $comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'hello' ),
+					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'schools' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'hello' ) ),
+					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'schools' ) ),
 					number_format_i18n( $comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
@@ -60,7 +60,7 @@ if ( post_password_required() ) {
 
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) : ?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'hello' ); ?></p>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'schools' ); ?></p>
 		<?php
 		endif;
 
--- single.php
+++ single.php
@@ -17,13 +17,111 @@
 
  					<?php while ( have_posts() ) : the_post(); ?>
 
+             <?php if ( has_post_thumbnail() ): // If has thumbnail image do the rest...?>
+             
+                                      <?php
+                                                     $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+                                               // $mobile_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'image-reg-name' );
+                                     ?>
+             
+                                  
+             
+                                     <!-- <img class="class-name" src="<?php echo $large_image_url[0]; ?>" width="<?php echo $large_image_url[1]; ?>" height="<?php echo $large_image_url[2]; ?>" alt="<?php echo $large_image_url['_wp_attachment_image_alt']['0']; ?>"> -->
+             
+                                     						<div class="heading-image" style="background-image: url('<?php echo $large_image_url[0]; ?>')">
+                                     							<h4><?php the_category(','); ?></h4>
+                                     							<h1><?php the_title();?></h1>
+                                     							<h5>Reading time 4mins</h5>
+                                     						</div> <!-- heading-image -->
+             
+                           <?php else :?>
+             
+                             						<div class="heading-image">
+                             							<h4><?php the_category(','); ?></h4>
+                             							<h1><?php the_title();?></h1>
+                             							<h5>Reading time 4mins</h5>
+                             						</div> <!-- heading-image -->
+             
+                           <?php endif; ?>
+             
+            <!-- Main text and image content -->
 						<div class="single-container">
+
+							<div class="author">
+								<?php echo get_avatar( get_the_author_meta( 'ID' ), 64 ); ?>
+								<h4> <b>By</b> <?php the_author(); ?></h4>
+								<h4><?php the_time('F j, Y'); ?></h4>
+							</div> <!-- author -->
 
 							<?php the_content();?>
 
-						</div>
+						</div> <!-- single-container -->
+
+
 
  					<?php endwhile; ?>
+
+
+          <!-- Related posts -->
+          <div class="related-container">
+
+    				<h3 class="related-posts">YOU MIGHT ALSO LIKE</h3>
+    				<hr></hr>
+
+    				<div class="row">
+
+    					<?php while ( have_posts() ) : the_post(); ?>
+
+    					<div class="item">
+    						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    							<a href="<?php the_permalink();?>">
+    								<?php the_post_thumbnail('post-intro-image'); ?>
+    							</a>
+    							<div class="item-text">
+    								<h5><?php the_category(','); ?></h5>
+    								<a href="<?php the_permalink();?>">
+    									<h2><?php the_title();?></h2>
+    								</a>
+    							</div> <!-- item-text -->
+    						</div> <!-- post -->
+    					</div> <!-- item -->
+
+              <div class="item">
+    						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    							<a href="<?php the_permalink();?>">
+    								<?php the_post_thumbnail('post-intro-image'); ?>
+    							</a>
+    							<div class="item-text">
+    								<h5><?php the_category(','); ?></h5>
+    								<a href="<?php the_permalink();?>">
+    									<h2><?php the_title();?></h2>
+    								</a>
+    							</div> <!-- item-text -->
+    						</div> <!-- post -->
+    					</div> <!-- item -->
+
+              <div class="item">
+    						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    							<a href="<?php the_permalink();?>">
+    								<?php the_post_thumbnail('post-intro-image'); ?>
+    							</a>
+    							<div class="item-text">
+    								<h5><?php the_category(','); ?></h5>
+    								<a href="<?php the_permalink();?>">
+    									<h2><?php the_title();?></h2>
+    								</a>
+    							</div> <!-- item-text -->
+    						</div> <!-- post -->
+    					</div> <!-- item -->
+
+
+    					<?php endwhile; ?>
+
+    				</div> <!-- row -->
+
+    			</div> <!-- container -->
+
+    		</main><!-- #main -->
 
  			<?php the_posts_pagination( array(
  				'mid_size' => 2,
