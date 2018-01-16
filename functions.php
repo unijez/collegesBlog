6367ae8ebbc7b46d6606674379f347dcdead04f6
Jez <jthompson@lincoln.ac.uk>
2018-01-16 16:11:21 +0000

    Link in site wide JS

--- functions.php
+++ functions.php
@@ -180,6 +180,10 @@ function collegeBlog_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+	
+	
+	wp_enqueue_script( 'site-scripts', get_template_directory_uri() . '/js/site-wide-min.js', array('jquery'), null, true );
+	
 }
 add_action( 'wp_enqueue_scripts', 'collegeBlog_scripts' );
 
