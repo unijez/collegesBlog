18d939a855d215259f10ecdbee0097dbf4e299a5
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-01-24 14:14:49 +0000

    Fixing: Centring the slider titles and text, fixing the nav text collapsing

--- template-parts/slider.php
+++ template-parts/slider.php
@@ -46,10 +46,15 @@
 
         <div class="overlay-slide item">
             <div class="item-text">
-              <h5><?php the_category(',');?></h5>
+              <h5 class="brighter"><?php the_category(',');?></h5>
               <a href="<?php the_permalink()?>">
                 <h2><?php the_title();?></h2>
               </a>
+              <div class="readmore">
+								<p><?php the_excerpt();?>
+                  <a href="<?php the_permalink()?>">Read More...</a>
+                </p>
+							</div> <!-- author -->
             </div> <!-- item-text -->
           </div> <!-- post -->
         </div> <!-- item -->
