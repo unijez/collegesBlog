1a6f9f2d87944f59693e200589d587b9c4c8e21c
Jez <jthompson@lincoln.ac.uk>
2018-01-24 12:32:27 +0000

    Fix search page.

--- search.php
+++ search.php
@@ -13,14 +13,55 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<div id="primary" class="content-area">
+
 		<main id="main" class="site-main">
 
-		Search Page
+			<?php if ( have_posts() ) : ?>
+
+			<div class="main-container">
+
+				<h3 class="latest-posts">LATEST POSTS</h3>
+				<hr></hr>
+
+				<div class="row">
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+					<div class="item">
+						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<a href="<?php the_permalink();?>">
+								<?php default_image('post-intro-image'); ?>
+							</a>
+							<div class="item-text">
+								<h5><?php the_category(','); ?></h5>
+								<a href="<?php the_permalink();?>">
+									<h2><?php the_title();?></h2>
+								</a>
+								<p><?php the_excerpt();?></p>
+							</div> <!-- item-text -->
+						</div> <!-- post -->
+					</div> <!-- item -->
+
+					<?php endwhile; ?>
+
+				</div> <!-- row -->
+
+			</div> <!-- container -->
+
+			<?php the_posts_pagination( array(
+				'mid_size' => 2,
+				'prev_text' => __( 'Back', 'textdomain' ),
+				'next_text' => __( 'Onward', 'textdomain' ),
+			) ); ?>
+
+			<?php endif;  wp_reset_query(); ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
 
-<?php
-get_sidebar();
+	</div><!-- #primary -->
+
+
+	<?php
+
 get_footer();
