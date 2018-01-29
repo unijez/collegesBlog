7aef4f84e509ba66062045303d3f22b683ee3228
Jez <jthompson@lincoln.ac.uk>
2018-01-29 11:29:12 +0000

    IE button fix

--- header.php
+++ header.php
@@ -55,11 +55,13 @@
 
 		
 		<button type="button" class="nav-button">
+				  	
+			<span class="site-icon">
+			  	<i class="fal fa-bars"></i>
+			  	<i class="fal fa-times"></i>
+			</span>
 		
-		          	<i class="fal fa-bars"></i>
-		          	<i class="fal fa-times"></i>
-		
-		          </button>
+		</button>
 		
 
         <nav class="global-nav retract">
