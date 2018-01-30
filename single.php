1eccf7002eeafe26728ddfbcabc143837c09c0a8
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-01-30 14:38:19 +0000

    Fixed: While loop error on single pages

--- single.php
+++ single.php
@@ -123,49 +123,49 @@
     								</a>
     							</div> <!-- item-text -->
     						<!--</div> <!-- post -->
-                <?php echo do_shortcode('[jetpack-related-posts]'); ?>
-    					</div> <!-- item -->
+                <!--<//?php echo do_shortcode('[jetpack-related-posts]'); ?>
+    					<!--</div> <!-- item -->
 
-              <div class="item">
-    						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    							<a href="<?php the_permalink();?>">
-    								<?php the_post_thumbnail('post-intro-image'); ?>
+              <!--<div class="item">
+    						<div id="post-<//?php the_ID(); ?>" <//?php post_class(); ?>>
+    							<a href="<//?php the_permalink();?>">
+    								<//?php the_post_thumbnail('post-intro-image'); ?>
     							</a>
     							<div class="item-text">
     								<h5>
-                      <?php $categories = get_the_category();
+                      <//?php $categories = get_the_category();
   									  if ( ! empty( $categories ) ) {
       								   echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
   									  }?>
                     </h5>
-    								<a href="<?php the_permalink();?>">
-    									<h2><?php the_title();?></h2>
+    								<a href="<//?php the_permalink();?>">
+    									<h2><//?php the_title();?></h2>
     								</a>
     							</div> <!-- item-text -->
-    						</div> <!-- post -->
-    					</div> <!-- item -->
+    						<!--</div> <!-- post -->
+    					<!--</div> <!-- item -->
 
-              <div class="item">
-    						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    							<a href="<?php the_permalink();?>">
-    								<?php default_image('post-intro-image'); ?>
+              <!--<div class="item">
+    						<div id="post-<//?php the_ID(); ?>" <//?php post_class(); ?>>
+    							<a href="<//?php the_permalink();?>">
+    								<//?php default_image('post-intro-image'); ?>
     							</a>
     							<div class="item-text">
     								<h5>
-                      <?php $categories = get_the_category();
+                      <//?php $categories = get_the_category();
   									  if ( ! empty( $categories ) ) {
       								   echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
   									  }?>
                     </h5>
-    								<a href="<?php the_permalink();?>">
-    									<h2><?php the_title();?></h2>
+    								<a href="<//?php the_permalink();?>">
+    									<h2><//?php the_title();?></h2>
     								</a>
     							</div> <!-- item-text -->
-    						</div> <!-- post -->
-    					</div> <!-- item -->
+    						<!--</div> <!-- post -->
+    					<!--</div> <!-- item -->
 
 
-    					<?php endwhile; ?>
+    					<!--<//?php endwhile; ?>-->
 
     				</div> <!-- row -->
 
