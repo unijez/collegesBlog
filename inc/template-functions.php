db2d7923bb6f096a857550aecd183a6909ebd5b3
Jez <jthompson@lincoln.ac.uk>
2018-01-30 14:50:16 +0000

    Turn off default comments js

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -142,3 +142,11 @@ function collegeBlog_search_form( $form ) {
 	return $form;
 }
 add_filter( 'get_search_form', 'collegeBlog_search_form', 100 );
+
+
+// DISABLE WORDPRESS COMMENTS JS
+
+function itsg_disable_comment_js(){
+    wp_deregister_script( 'comment-reply' );
+}
+add_action( 'init', 'itsg_disable_comment_js' );
