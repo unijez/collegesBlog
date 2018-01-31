6e9dfb2514e2080d83ac358f85cd1d34889cd23a
Thomas Moore <tom.moore292@gmail.com>
2018-01-31 12:33:51 +0000

    pagination finished

--- index.php
+++ index.php
@@ -59,9 +59,9 @@ get_header(); ?>
 			</div> <!-- container -->
 
 			<?php the_posts_pagination( array(
-				'mid_size' => 2,
-				'prev_text' => __( 'Back', 'textdomain' ),
-				'next_text' => __( 'Onward', 'textdomain' ),
+				'mid_size' => 1,
+				'prev_text' => __( 'Prev', 'textdomain' ),
+				'next_text' => __( 'Next', 'textdomain' ),
 			) ); ?>
 
 			<?php endif;  wp_reset_query(); ?>
