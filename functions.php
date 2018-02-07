b05aeb0ac86911f969b5b11c3a206725c6f7e756
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-02-07 13:10:25 +0000

    Fixed: Adding figure around the images rather than completely removing the p tag

--- functions.php
+++ functions.php
@@ -248,9 +248,20 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 /*
  * ACF P tag from image stripping
  */
-function filter_ptags_on_images($content) {
+/*function filter_ptags_on_images($content) {
  	$content = preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 	return preg_replace('/<p>\s*(<iframe .*>*.<\/iframe>)\s*<\/p>/iU', '\1', $content);
 }
 add_filter('acf_the_content', 'filter_ptags_on_images');
-add_filter('the_content', 'filter_ptags_on_images');
+add_filter('the_content', 'filter_ptags_on_images');*/
+
+function fb_unautop_4_img( $content )
+{
+    $content = preg_replace(
+        '/<p>\\s*?(<a rel=\"attachment.*?><img.*?><\\/a>|<img.*?>)?\\s*<\\/p>/s',
+        '<figure class="wp-caption alignnone">$1</figure>',
+        $content
+    );
+    return $content;
+}
+add_filter( 'the_content', 'fb_unautop_4_img', 99 );
