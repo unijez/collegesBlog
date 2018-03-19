3ead97d18085eea42a30e8c3671009bf4583a0e4
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-03-19 12:02:02 +0000

    Fixed: Side bar weirdness where li's would blend into each other

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ get_header(); ?>
 				<hr></hr>
 
 				<div class="row site-module-inner">
-					<?php $query = new WP_Query('cat='.get_cat_ID('Research').' & post_per_page=-1'); $counter = 1; ?>
+					<?php $query = new WP_Query('cat='.get_cat_ID('Research').' & post_per_page=3'); $counter = 1; ?>
 
 					<?php while ( $query->have_posts() && !( $counter > 3)  ) : $query->the_post(); ?>
 
@@ -49,7 +49,7 @@ get_header(); ?>
 				<hr></hr>
 
 				<div class="row site-module-inner">
-					<?php $query = new WP_Query('cat='.get_cat_ID('Teaching and Learning').'&post_per_page=-1'); $counter = 1; ?>
+					<?php $query = new WP_Query('cat='.get_cat_ID('Teaching and Learning').'&post_per_page=3'); $counter = 1; ?>
 					<?php while ( $query->have_posts() && !( $counter > 3)  ) : $query->the_post(); ?>
 
 					<div class="item">
@@ -70,7 +70,7 @@ get_header(); ?>
 				<hr></hr>
 
 				<div class="row site-module-inner">
-					<?php $query = new WP_Query('cat='.get_cat_ID('News').'&post_per_page=-1'); $counter = 1; ?>
+					<?php $query = new WP_Query('cat='.get_cat_ID('News').'&post_per_page=3'); $counter = 1; ?>
 					<?php while ( $query->have_posts() && !( $counter > 3 ) ) : $query->the_post(); ?>
 					<div class="item">
 
@@ -90,7 +90,7 @@ get_header(); ?>
 				<hr></hr>
 
 				<div class="row site-module-inner">
-					<?php $query = new WP_Query('cat='.get_cat_ID('Events').'&post_per_page=-1'); $counter = 1?>
+					<?php $query = new WP_Query('cat='.get_cat_ID('Events').'&post_per_page=3'); $counter = 1;?>
 
 					<?php while ( $query->have_posts() && !( $counter > 3)  ) : $query->the_post(); ?>
 
